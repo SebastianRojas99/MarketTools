1b1553a582784438d5e0eefa2b3c63acb56412b1
Sebastian <96402832+SebastianRojas99@users.noreply.github.com>
2024-04-07 22:13:43 -0300

    Initial Commit

---
+++ MarketTools/MarketToolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarketToolsApp.swift
+//  MarketTools
+//
+//  Created by Sebastian Marquez Rojas on 07/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct MarketToolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
